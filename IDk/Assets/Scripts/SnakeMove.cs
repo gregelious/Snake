ed8a375a953604d89ef0b66fe6d20cc1f686f12b
gregelious <iftakar201@gmail.com>
2023-09-26 11:23:01 -0400

    Update SnakeMove.cs

--- IDk/Assets/Scripts/SnakeMove.cs
+++ IDk/Assets/Scripts/SnakeMove.cs
@@ -7,10 +7,14 @@ public class SnakeMove : MonoBehaviour
 
     private Vector2 direction;
 
+    List<Transform> segments; // stores all parts of the body of the snake
+    public Transform bodyPrefab; // variable to store the body
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        segments = new List<Transform>(); // create new list
+        segments.Add(transform); // add head of snake to the list
     }
 
     // Update is called once per frame
@@ -41,5 +45,13 @@ public class SnakeMove : MonoBehaviour
             Mathf.Round(this.transform.position.x) + direction.x,
             Mathf.Round(this.transform.position.y) + direction.y
             );
+    }
+
+    //fxn to make the snake grow
+    void Grow()
+    {
+        Transform segment = Instantiate(this.bodyPrefab); // create new body part
+        segment.position = segments[segments.Count - 1].position; // position it to the back of snake
+        segments.Add(segment); // add it to the list
     }
 }
