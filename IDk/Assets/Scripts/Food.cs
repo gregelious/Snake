bc33c89c860bfddd05c01c854b98a0a907dc1888
gregelious <iftakar201@gmail.com>
2023-09-28 11:49:58 -0400

    Update Food.cs

--- IDk/Assets/Scripts/Food.cs
+++ IDk/Assets/Scripts/Food.cs
@@ -18,4 +18,15 @@ public class Food : MonoBehaviour
     {
         
     }
+
+    // fxn to randomize the position of the food
+    private void RandomPos()
+    {
+        Bounds bounds = grid.bounds; // declare limits of the space
+
+        float x = Random.Range(bounds.min.x, bounds.max.x); // random x in limit
+        float y = Random.Range(bounds.min.y, bounds.max.y); // random y in limit
+
+        transform.position = new Vector2(Mathf.Round(x), Mathf.Round(y)); // round values and change position
+    }
 }
